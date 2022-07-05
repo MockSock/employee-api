91407505d3d7040fa0154c703b361dd37d94ff68
madden20202 <business.mocksock@gmail.com>
2022-07-05 10:36:01 -0500

    __name__ doesnt need ""

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
-if "__name__" == "__main__": 
+if __name__ == "__main__": 
     app.run(debug=True)
 
 @app.route("/")
