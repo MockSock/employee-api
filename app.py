6b5f3dfef0ce10277b1ba8a1d1739a219c39b7f4
madden20202 <business.mocksock@gmail.com>
2022-07-05 10:33:10 -0500

    add employee.db file and then start tinkering

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("__name__")
 
+
 if "__name__" == "__main__": 
     app.run(debug=True)
 
 @app.route("/")
 def hello_world():
     return "Hello World"
+
+@app.route("/employees")
+def getEmployees():
+
+    return "I will be a get method soon"
