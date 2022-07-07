646b2009e549382923aec154c8455b5f4795c7e2
madden20202 <business.mocksock@gmail.com>
2022-07-07 11:39:47 -0500

    add marshmallow work

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.db'
 
 #  Database Work
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 class Employee(db.Model):
     employee_id = db.Column(db.Integer, primary_key=True)
 
+class EmployeeSchema(ma.Schema):
+    class Meta:
+        fields = ['']
+
+employee_schema = EmployeeSchema(many=True)
 
 if __name__ == "__main__": 
     app.run(debug=True)
