d20bde5172792048551a3e4e11c8f408ff5f8a7f
madden20202 <business.mocksock@gmail.com>
2022-07-07 11:35:25 -0500

    add imports that will be used later

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from tokenize import String
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+# Config work
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.db'
+
+#  Database Work
 db = SQLAlchemy(app)
 
-class EmployeeList(db.Model):
+class Employee(db.Model):
     employee_id = db.Column(db.Integer, primary_key=True)
-    
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
