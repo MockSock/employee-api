9dc2ba5a1481a485ca7b54da8760e724727c5abf
madden20202 <business.mocksock@gmail.com>
2022-07-07 11:28:27 -0500

    make employee list class and id

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+from tokenize import String
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+
+class EmployeeList(db.Model):
+    employee_id = db.Column(db.Integer, primary_key=True)
+    
 
 if __name__ == "__main__": 
     app.run(debug=True)
