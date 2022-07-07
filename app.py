874c1abe9098b8a3be2bb5a73acce098802a3e7c
madden20202 <business.mocksock@gmail.com>
2022-07-07 11:48:07 -0500

    flesh out employee model and schema

--- app.py
+++ app.py
@@ -12,10 +12,13 @@ ma = Marshmallow(app)
 
 class Employee(db.Model):
     employee_id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(50))
+    role = db.Column(db.String(50))
+    employment_type = db.Column(db.String(50))
 
 class EmployeeSchema(ma.Schema):
     class Meta:
-        fields = ['']
+        fields = ('employee_id', 'full_name', 'role', 'employment_type')
 
 employee_schema = EmployeeSchema(many=True)
 
