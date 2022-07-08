57829351532de78bd4e10103f26b46c19990032e
madden20202 <business.mocksock@gmail.com>
2022-07-08 09:58:27 -0500

    give notes to have this run on local host

--- app.py
+++ app.py
@@ -24,7 +24,9 @@ employee_schema = EmployeeSchema(many=True)
 
 if __name__ == "__main__":
     db.create_all() 
-    app.run(host="localhost")
+    # to make the app run on local host, use command 
+    # flask run -h localhost -p 5000
+    app.run()
 
 @app.route("/")
 def hello_world():
